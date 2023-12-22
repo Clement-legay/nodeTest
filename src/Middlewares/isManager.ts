911df8ecfe320c21a0a728b5139dcb405cb0fd69
Clément Legay <delta80@outlook.fr>
2023-12-22 15:13:30 +0100

    fix: manager authorizations are inferior to admin's one

--- src/Middlewares/isManager.ts
+++ src/Middlewares/isManager.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 
 const isAdminMiddleware = (req: Request, res: Response, next: NextFunction) => {
-    if (req.body.user.role !== "Manager") {
+    if (req.body.user.role !== "Manager" && req.body.user.role !== "Admin") {
         res.status(403).json({ error: 'Forbidden' });
     } else {
         next();
